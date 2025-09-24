9c11d41bf291326b2c2c8f1e9dd98d053e1a66ca
alarninjad <alarninjad@gmail.com>
2025-09-24 11:00:46 +0330

    authentication is done

--- Exchange/urls.py
+++ Exchange/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from transaction.views import home
+from authentication.views import login,register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home, name="recipes"),
+    path('home/', home, name="home"),
+    path('login/', login, name="login"),
+    path('register/', register, name="register"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def register(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('pasword')
+        # getting username and password
+    
+        if User.objects.filter(username=username).exists():
+            return redirect('/login/')
+        # if its already registered
+
+        user = User.objects.create_user(username=username)
+        # create username
+
+        user.set_password(password)
+        user.save()
+        # set password and save
+
+        return redirect('/login/')
+        # now its time to log in
+
     return render(request,'register.html')
+
+
+def login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('pasword')
+        # getting username and password
+
+        if not User.objects.filter(username=username).exists():
+            return redirect('/register/')
+        # if there is not that username
+
+        user = authenticate(username=username,password=password)
+        # authenticaion
+
+        if user:
+            login(request,user)
+            return redirect('/home/')
+        else:
+            return redirect('/login/')
+        # login
+
+    return render(request,'login.html')
