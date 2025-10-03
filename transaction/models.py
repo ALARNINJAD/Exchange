87bf9296f04ed04df669ba4873ce1e7445f70d07
alarninjad <alarninjad@gmil.com>
2025-10-03 17:55:42 +0330

    transaction logic completed

--- transaction/models.py
+++ transaction/models.py
@@ -18,5 +18,5 @@ class price(models.Model):
 
     # currencies prices
     toman = models.DecimalField(max_digits=8, decimal_places=2, default=1)
-    dollor = models.DecimalField(max_digits=8, decimal_places=2, default=115)
-    pounds = models.DecimalField(max_digits=8, decimal_places=2, default=155)
+    dollar = models.DecimalField(max_digits=8, decimal_places=2, default=115)
+    pound = models.DecimalField(max_digits=8, decimal_places=2, default=155)
--- transaction/views.py
+++ transaction/views.py
@@ -12,14 +12,45 @@ def home(request):
     user_wallet, created = wallet.objects.get_or_create(user=request.user)
 
     if request.method == 'POST':
-        
-        # adding balance to the wallet
-        add_toman = request.POST.get('add_toman')
-        if add_toman:
-            user_wallet.toman += Decimal(add_toman)
+        if 'add_toman' in request.POST:
+
+            # adding balance to the wallet
+            add_toman = request.POST.get('add_toman')
+            if add_toman:
+                user_wallet.toman += Decimal(add_toman)
+                user_wallet.save()
+
+        elif 'from_currency' or 'to_currency' in request.POST:
+
+            # getting transactions inputs
+            from_currency = request.POST.get('from-currency')
+            to_currency = request.POST.get('to-currency')
+            amount = Decimal(request.POST.get('amount'))
+
+            # setting a map for different options
+            current_price = price.objects.first()
+            rates = {
+                'toman': {
+                    'dollar': Decimal(1) / current_price.dollar,
+                    'pound': Decimal(1) / current_price.pound,
+                    'toman': Decimal(1),
+                },
+                'dollar': {
+                    'toman': current_price.dollar,
+                    'pound': current_price.dollar / current_price.pound,
+                    'dollar': Decimal(1),
+                },
+                'pound': {
+                    'toman': current_price.pound,
+                    'dollar': current_price.pound / current_price.dollar,
+                    'pound': Decimal(1),
+                }
+            }
+
+            # calculate
+            result = amount * rates[from_currency][to_currency]
+            setattr(user_wallet, from_currency, getattr(user_wallet, from_currency) - amount)
+            setattr(user_wallet, to_currency, getattr(user_wallet, to_currency) + result)
             user_wallet.save()
-
-        
-
 
     return render(request,'home.html',{"user_wallet":user_wallet})
